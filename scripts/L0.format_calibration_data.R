c62202435fcdb5d324807a4b8edc4d5211b01e0a
kdorheim <kalyn.dorheim@pnnl.gov>
2025-07-31 21:29:59 -0400

    ch4 obs

--- scripts/L0.format_calibration_data.R
+++ scripts/L0.format_calibration_data.R
@@ -43,6 +43,38 @@ noaa_n2o %>%
 
 ## 1B. CH4  --------------------------------------------------------------------
 
+# Load the CH4 NOAA observations
+fname <- file.path(DIRS$RAW_DATA, "ch4_annmean_gl.csv")
+stopifnot(file.exists(fname))
+
+read.csv(fname, comment.char = "#") %>%
+    select(year, value = mean) %>%
+    mutate(variable = CONCENTRATIONS_CH4(),
+           source = "NOAA") ->
+    noaa_ch4
+
+
+# Load the CH4 observations from RCMIP.
+system.file(package  = "hector", "input/tables") %>%
+    file.path("ssp245_emiss-constraints_rf.csv") %>%
+    read.csv(comment.char = ";") %>%
+    filter(Date <= max(noaa_ch4$year)) %>%
+    select(year = Date, value = CH4_CONSTRAIN()) %>%
+    mutate(variable = CONCENTRATIONS_CH4(),
+           source = "RCMIP") %>%
+    filter(year < min(noaa_ch4$year)) ->
+    rmcip_obs
+
+
+# Combine the NOAA and RCMIP values.
+noaa_ch4 %>%
+    rbind(rmcip_obs) %>%
+    select(year, value, variable) %>%
+    mutate(units = getunits(CH4_CONSTRAIN())) %>%
+    arrange(year) ->
+    ch4_obs
+
+
 
 
 # Z. Save Results --------------------------------------------------------------
